ccd53fe000aeb27b786b6380915a2ea629874d77
guicodee <guilhermedfgo12@gmail.com>
2026-06-29 19:04:57 -0300

    feat: add new variant in button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        groups: "border cursor-pointer border-acent-primary bg-background text-zinc-300 rounded-md hover:bg-secondary/50 hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-transparent dark:hover:bg-input/30"
       },
       size: {
         default:
